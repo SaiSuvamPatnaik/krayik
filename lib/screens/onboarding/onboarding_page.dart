c4a4cfd82d65a5be54767c665bb613415619b680
mehul-tech <goel.mehul19@gmail.com>
2021-06-03 18:47:00 +0530

    first commit

--- lib/screens/onboarding/onboarding_page.dart
+++ lib/screens/onboarding/onboarding_page.dart
@@ -7,7 +7,6 @@ class OnboardingPage extends StatelessWidget {
   static String routeName = "/splash";
   @override
   Widget build(BuildContext context) {
-    // You have to call it on your starting screen
     SizeConfig().init(context);
     return Scaffold(
       body: Body(),
